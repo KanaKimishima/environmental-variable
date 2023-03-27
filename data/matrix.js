e91d43cd3dd0a57ecda126f3768c717b1ede021f
KanaKimishima <kana.kimishima.work@gmail.com>
2023-03-27 10:28:37 -0700

    first commit

---
+++ data/matrix.js
@@ -0,0 +1,32 @@
+export const matrix = {
+    courses: [
+        {
+            term: 1,
+            code: "COMM 1118",
+            title: "Communication for Digital Design and Development",
+            credits: 4.0,
+            description: "This is a description..."
+        },
+        {
+            term: 2,
+            code: "COMM 2218",
+            title: "Communication for Digital Design and Development",
+            credits: 4.0,
+            description: "This is a description..."
+        },
+        {
+            term: 3,
+            code: "COMP 3170",
+            title: "Front end web development",
+            credits: 3.0,
+            description: "This is a description..."
+        },
+        {
+            term: 4,
+            code: "BLAW 3205",
+            title: "Internet and IT Law",
+            credits: 3.0,
+            description: "This is a description..."
+        }
+    ]
+}
